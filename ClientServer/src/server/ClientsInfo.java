f0e3c4b05cacc7298a5c240bf995525c364017ce
Ünal Asil <unalyildiz37@gmail.com>
2020-09-24 14:04:05 +0300

    Adding connected clients info.

--- ClientServer/src/server/ClientsInfo.java
+++ ClientServer/src/server/ClientsInfo.java
@@ -1,5 +1,34 @@
 package server;
 
+import java.net.Socket;
+
 public class ClientsInfo { //This class holds the connected clients information.
 //TODO Hold connected clients informations.
+	private Socket socket;
+	
+	private String hostName;
+
+	private String hostAdress;
+	
+	public ClientsInfo(Socket socket) {
+		this.socket = socket;
+		this.hostName  = socket.getInetAddress().getHostName();
+		this.hostAdress  = socket.getInetAddress().getHostAddress();
+	}
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public String getHostAdress() {
+		return hostAdress;
+	}
+
+	public void setHostAdress(String hostAdress) {
+		this.hostAdress = hostAdress;
+	}
 }
--- ClientServer/src/server/Server.java
+++ ClientServer/src/server/Server.java
@@ -2,11 +2,12 @@ package server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 public class Server {
 	
 
 	ServerWorker worker;
-	
+	static ArrayList<ClientsInfo> clients = new ArrayList<ClientsInfo>();
 	
 	public static void main(String[] args) {
 		try {
@@ -15,13 +16,21 @@ public class Server {
 			while(true) {
 				Socket socket = serverSocket.accept();
 				System.out.println("Connection Establishment to Client");
-				
+				clients.add(new ClientsInfo(socket));
+				getClientsInfo();
 				ServerWorker worker = new ServerWorker(socket);
 				worker.start(); // Multi Client support.
 			}
 		}
 		catch(Exception e){
 			e.printStackTrace();
+		}
+	}
+
+	private static void getClientsInfo() {
+		// TODO Auto-generated method stub
+		for(ClientsInfo client: clients) {
+			System.out.println("Connected clients name : " + client.getHostName() + " adress : " + client.getHostAdress());
 		}
 	}
 }

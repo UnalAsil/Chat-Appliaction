ce2b91b7ed1310377afa97b3726c00da2c794385
Ünal Asil <unalyildiz37@gmail.com>
2020-10-19 11:20:15 +0300

    Adding log

--- ClientServer/src/client/Client.java
+++ ClientServer/src/client/Client.java
@@ -7,29 +7,37 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import message.*;
 
 public class Client {
-	static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
-	 private static ObjectOutputStream outputStream = null;
-	Socket socket ;
+	
+	private static final Logger LOGGER = Logger.getLogger( Client.class.getName() );
+	
+	private static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+	private static ObjectOutputStream outputStream = null;
+	private Socket socket ;
 
 	public Client() throws IOException {
+		
 		connect();
 		outputStream = new ObjectOutputStream(socket.getOutputStream());
 
 	}
+	
 	public void connect(){
 		//TODO Servere baglan.
 		try {
 			socket = new Socket("localhost", 9806);
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+		catch (UnknownHostException e) 
+		{
+			LOGGER.log( Level.SEVERE, e.toString(), e );	
+		}
+		catch (IOException e) {
+			LOGGER.log( Level.SEVERE, e.toString(), e );	
 		}
 		System.out.println("Client Conncected");
 	}
@@ -38,28 +46,32 @@ public class Client {
 		//TODO Gelen mesaj var mi diye servere sor.
 	}
 	
-	
 	public void closeConnection() {
-		//TODO Server baglantisini kapat.
 		try {
+			
 			socket.close();
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
+			LOGGER.log( Level.SEVERE, e.toString(), e );	
+			
 		}
 		System.out.println("Conservation ended.");
 	}
 	 
 	public void sendMessage (String to, String cc, String subject, String priorty, String message , String from ) {
+		
 		Message mes = new Message(to,cc,subject,priorty, message , from);
+		
 		try {
-
+			
 			outputStream.writeObject(mes); //Send to Server message object.
 			outputStream.reset();
+			
 		}
 		catch(Exception e){
-			e.printStackTrace();
+			LOGGER.log( Level.SEVERE, e.toString(), e );	
 		}
+		
 	}
-	
 }

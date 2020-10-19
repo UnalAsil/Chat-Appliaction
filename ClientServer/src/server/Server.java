83e6410d32a643b375f4deddc704bb727fe37e11
Ünal Asil <unalyildiz37@gmail.com>
2020-10-19 11:09:40 +0300

    Adding log

--- ClientServer/src/server/Server.java
+++ ClientServer/src/server/Server.java
@@ -1,36 +1,55 @@
 package server;
 
 import java.net.ServerSocket;
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+
 import java.net.Socket;
 import java.util.ArrayList;
 public class Server {
 	
 
-	ServerWorker worker;
-	static ArrayList<ClientsInfo> clients = new ArrayList<ClientsInfo>();
+	private ServerWorker worker;
+	private static ArrayList<ClientsInfo> clients = new ArrayList<ClientsInfo>();
 	
 	public static void main(String[] args) {
+		
 		try {
+			
+		       Logger logger 
+	            = Logger.getLogger( 
+	            		Server.class.getName()); 
+	  
 			System.out.println("Waiting for clients");
 			ServerSocket serverSocket = new ServerSocket(9806);
-			while(true) {
+			
+			logger.log(Level.INFO, "Server started.");
+			
+			while (true) {
+				
 				Socket socket = serverSocket.accept();
-				System.out.println("Connection Establishment to Client");
 				clients.add(new ClientsInfo(socket));
-				getClientsInfo();
+				logger.log(Level.INFO, "Connection Establishment to Client");
+
+				connectedClientsInfo();
 				ServerWorker worker = new ServerWorker(socket);
 				worker.start(); // Multi Client support.
 			}
+			
 		}
-		catch(Exception e){
+		catch (Exception e){
+			
 			e.printStackTrace(); 
+			
 		}
 	}
 
-	private static void getClientsInfo() {
-		// TODO Auto-generated method stub
+	private static void connectedClientsInfo() {
+		
 		for(ClientsInfo client: clients) {
+			
 			System.out.println("Connected clients name : " + client.getHostName() + " adress : " + client.getHostAdress());
+			
 		}
 	}
 }
